21e6759e5684b09589142c605c38465a67be6bca
AleCortinovis <114218535+AleCortinovis@users.noreply.github.com>
2022-10-03 09:58:46 +0200

    Create ScientificCalculator.java

---
+++ codice/Calcolatori/src/ScientificCalculator.java
@@ -0,0 +1,7 @@
+
+public class ScientificCalculator extends Prova_calcolatori_git {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("ciao sciemtifica");
+	}
+}
